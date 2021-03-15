f6a3e6d12a23821cebd301b8cffacd70861147f9
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-15 16:48:14 +0100

    aggiunte funzioni mouse enter/ mouse leave

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ $(".main-nav li").click(function() {
   $(this).children(".dropdown-menu").slideToggle();
 }
 );
+// funzione mouse enter mostra il menu
+$(".main-nav li").mouseenter(function() {
+  $(this).children(".dropdown-menu").show("slow");
+}
+);
+// funzione mouse leave scompare il menu
+$(".main-nav li").mouseleave(function() {
+  $(this).children(".dropdown-menu").hide("slow");
+}
+);
