4bb2ad1a854cf16a418415c3300ff2467dbae399
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-15 17:41:50 +0100

    aggiunti commenti e modifica funzione js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// funzione per far visualizzare il menu al click
-$(".main-nav li").click(function() {
-  $(this).children(".dropdown-menu").slideToggle();
-}
-);
-// funzione mouse enter mostra il menu
-$(".main-nav li").mouseenter(function() {
-  $(this).children(".dropdown-menu").show("slow");
-}
-);
-// funzione mouse leave scompare il menu
-$(".main-nav li").mouseleave(function() {
-  $(this).children(".dropdown-menu").hide("slow");
-}
-);
+$(function (){
+  // funzione per far visualizzare il menu al click
+  $(".main-nav li").click(function() {
+    $(this).children(".dropdown-menu").slideToggle();
+  }
+  );
+  // funzione mouse enter mostra il menu
+  $(".main-nav li").mouseenter(function() {
+    $(this).children(".dropdown-menu").show("slow");
+  }
+  );
+  // funzione mouse leave scompare il menu
+  $(".main-nav li").mouseleave(function() {
+    $(this).children(".dropdown-menu").hide("slow");
+  }
+  );
+});
